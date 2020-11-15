cd79cd9484e421b4f0d98a28bd3afee94d95998a
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-15 06:08:46 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,5 +9,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-puts "Where would you like to go?"
-input = gets.strip
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, position, player="X")
+  board[position] = player
+  
+end
